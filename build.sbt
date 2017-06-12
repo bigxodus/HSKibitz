2b7cdf0ad97db4d6ec49907d6058adf052ed5806
Wayne <waynejo1024@gmail.com>
2017-06-12 22:00:59 +0900

    parse cards.json

--- build.sbt
+++ build.sbt
@@ -3,4 +3,7 @@ name := "HSKibitz"
 version := "1.0"
 
 scalaVersion := "2.12.2"
-        
+
+libraryDependencies ++= Seq(
+    "org.json4s" %% "json4s-native" % "3.5.2"
+)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,13 @@
+import model.Card
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
 object Main {
     def main(args:Array[String]): Unit = {
+        implicit val formats = DefaultFormats
 
+        val resourcesPath = getClass.getResource("/cards.json")
+        val json = io.Source.fromFile(resourcesPath.getPath).mkString
+        val cards = parse(json).extract[List[Card]]
     }
 }
